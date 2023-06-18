f61d493076f206072c1b9d1b997f43bcda9540ae
Kelvin Kapaya <kalvinkapu@gmail.com>
2023-06-18 16:43:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,54 @@
 
 // Write your code here
+// const multiply= function (num1,num2){
+//      return num1*num2 }
+// let multiply =62
+// const multiply= 62
+// if (multiply===num1*num2) {console.log(num1*num2)
+// // }
+
+// let num1= 'x';
+// let num2= 'y';
+// ('x'*'y')=multiply
+// console.log(num1,num2)
+// console.log(31,33)
+// console.log((num1*num2)=multiply)
+// let multiply; Number
+// const $multiply= 62;
+// = num1*num2
+// let num1=Number;
+// let num2=Number;
+// const multiply= 62;
+
+// console.log(num1*num2 )= multiply
+// // console.log(31,num2)  
+var num1=31
+var num2=2
+
+const multiply= 62;
+
+console.log((num1*num2)===multiply)
+// let num3;
+// let num4;
+//  Math.random();
+// console.log(Math.floor(Math.random() * 10))
+let random = Math.floor(Math.random() * 10) + 1;
+
+// let mod = ((num3 % num4)===4)
+// if (mod === 4) { 
+//     return ('false') 
+// }
+// let num3=16
+// let num4=8
+// let mod = num3 % num4===4
+// if ( num3 % num4=== 4) { 
+//     console.log(0==4);  
+// }
+let num3=16
+let num4=4
+let mod = num3 % num4; // returns 4   
+if (mod = 4) { 
+    console.log('false'); 
+}
+
+let max = Math.max(1,2,3,5,8,10,15,20); // returns 20
